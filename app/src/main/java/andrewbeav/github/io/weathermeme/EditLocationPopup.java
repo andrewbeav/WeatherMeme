597fd2ee7115dac3f7839a9af67f2a77eaefd920
andrewbeav <andrewbevelhymer@gmail.com>
2017-03-12 11:24:28 -0500

    Make layout smaller and more like a popup window

--- app/src/main/java/andrewbeav/github/io/weathermeme/EditLocationPopup.java
+++ app/src/main/java/andrewbeav/github/io/weathermeme/EditLocationPopup.java
@@ -3,17 +3,27 @@ package andrewbeav.github.io.weathermeme;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.DisplayMetrics;
 
 /**
  * Created by andrewbeav on 3/12/17.
  */
 
 class EditLocationPopup extends Activity {
+    public static final double WIDTH_PERCENT = 0.8;
+    public static final double HEIGHT_PERCENT = 0.8;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.edit_location_popup);
 
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
 
+        int height = dm.heightPixels;
+        int width = dm.widthPixels;
+
+        this.getWindow().setLayout((int) (width*WIDTH_PERCENT), (int) (height*HEIGHT_PERCENT));
     }
 }
