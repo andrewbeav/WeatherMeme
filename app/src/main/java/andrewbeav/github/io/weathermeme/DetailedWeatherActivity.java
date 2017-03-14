8203c26e635f8b1854675a6d0c5896ef256f8de0
andrewbeav <andrewbevelhymer@gmail.com>
2017-03-14 14:26:47 -0500

    Add new activity

---
+++ app/src/main/java/andrewbeav/github/io/weathermeme/DetailedWeatherActivity.java
@@ -0,0 +1,13 @@
+package andrewbeav.github.io.weathermeme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetailedWeatherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detailed_weather);
+    }
+}
